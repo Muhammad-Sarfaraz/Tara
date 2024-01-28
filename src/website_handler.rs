69bc75929c7abbfcb438e5c8d2b6c06dc9cc18a6
githubaction009 <140196313+githubaction009@users.noreply.github.com>
2024-01-28 22:52:38 +0600

    initial commit

---
+++ src/website_handler.rs
@@ -0,0 +1,39 @@
+use super::http::{Method, Request, Response, StatusCode};
+use super::server::Handler;
+use std::fs;
+
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!(".\\public\\{}", file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                fs::read_to_string(path).ok()
+            }
+            Err(_) => None,
+        }
+    }
+}
+
+impl Handler for WebsiteHandler {
+    fn handle_request(&mut self, request: &Request) -> Response {
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::Ok, self.read_file("404.html")),
+                },
+            },
+            _ => Response::new(StatusCode::Ok, self.read_file("404.html")),
+        }
+    }
+}
